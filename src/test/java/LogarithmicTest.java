bcbfc297bd5ffc9967bc31bf3eff942004206923
Daniil Demichev <daniil_demichev@mail.ru>
2021-04-06 20:24:50 +0300

    Finish (Поправлены значения тестов)

--- src/test/java/LogarithmicTest.java
+++ src/test/java/LogarithmicTest.java
@@ -8,25 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LogarithmicTest {
     final double eps = 0.00001;
     @ParameterizedTest
-    @ValueSource(doubles = {0.0, 0.3, 1.0, 1.5, 3.0})
+    @ValueSource(doubles = {0.0, 0.5, 1.0, 2.0, 3.5})
     void custom_ln_check(double x) {
         Ln ln = new Ln(eps);
         assertEquals(ln.stubCalculate(x), ln.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {0.0, 0.3, 1.0, 1.5, 3.0})
+    @ValueSource(doubles = {0.0, 0.5, 1.0, 2.0, 3.5})
     void custom_log2_check(double x) {
         Log log2 = new Log(2,eps);
         assertEquals(log2.stubCalculate(x), log2.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {0.0, 0.3, 1.0, 1.5, 3.0})
+    @ValueSource(doubles = {0.0, 0.5, 1.0, 2.0, 3.5})
     void custom_log3_check(double x) {
         Log log3 = new Log(3, eps);
         assertEquals(log3.stubCalculate(x), log3.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {0.0, 0.3, 1.0, 1.5, 3.0})
+    @ValueSource(doubles = {0.0, 0.5, 1.0, 2.0, 3.5})
     void custom_log10_check(double x) {
         Log log10 = new Log(10, eps);
         assertEquals(log10.stubCalculate(x), log10.calculate(x), eps);
--- src/test/java/TrigonometryTest.java
+++ src/test/java/TrigonometryTest.java
@@ -7,32 +7,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TrigonometryTest {
     final double eps = 0.000001;
     @ParameterizedTest
-    @ValueSource(doubles = {-Math.PI/6 - 2*Math.PI, -Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI, Math.PI/6+2*Math.PI})
+    @ValueSource(doubles = {-Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI})
     void custom_sin_check(double x) {
         Sin sin = new Sin(eps);
         assertEquals(sin.stubCalculate(x), sin.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {-Math.PI/6 - 2*Math.PI, -Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI, Math.PI/6+2*Math.PI})
+    @ValueSource(doubles = {-Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI})
     void custom_cos_check(double x) {
         Cos cos = new Cos(eps);
         assertEquals(cos.stubCalculate(x), cos.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {-Math.PI/6 - 2*Math.PI, -Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI, Math.PI/6+2*Math.PI})
+    @ValueSource(doubles = {-Math.PI, -2*Math.PI/3, -Math.PI/2, -Math.PI/3, 0, Math.PI/3, Math.PI/2, 2*Math.PI/3, Math.PI})
     void custom_sec_check(double x) {
         Sec sec = new Sec(eps);
         assertEquals(sec.stubCalculate(x), sec.calculate(x), eps);
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {-Math.PI/6 - 2*Math.PI, -Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI, Math.PI/6+2*Math.PI})
+    @ValueSource(doubles = {-Math.PI/2, -Math.PI/3, -Math.PI/6, 0, Math.PI/6, Math.PI/3, Math.PI/2})
     void custom_tan_check(double x) {
         Tan tan = new Tan(eps);
         assertEquals(tan.stubCalculate(x), tan.calculate(x), eps);
     }
     @ParameterizedTest
-    @ValueSource(doubles = {-Math.PI/6 - 2*Math.PI, -Math.PI, -Math.PI/2, -Math.PI/6, 0, Math.PI/6, Math.PI/2, Math.PI, Math.PI/6+2*Math.PI})
+    @ValueSource(doubles = {-Math.PI/2, -Math.PI/3, -Math.PI/6, 0, Math.PI/6, Math.PI/3, Math.PI/2})
     void custom_cot_check(double x) {
         Cot cot = new Cot(eps);
         assertEquals(cot.stubCalculate(x), cot.calculate(x), eps);
